4fb2f856fb71bd9e3777417934752e7f76276577
Vien Mai <mienvanmaidt2@gmail.com>
2023-02-03 18:51:19 +0100

    Add test folder

---
+++ ndt/test/ndt_test.cpp
@@ -0,0 +1,16 @@
+#include "ndt/ndt.hpp"
+#include "gtest/gtest.h"
+
+class NDTLocalizationTest : public localization::NDTLocalization {
+};
+
+TEST(ndt_test, get_initial_pose) {
+  double eps = 0.0001f;
+  ASSERT_NEAR(0, 0, eps);
+}
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
