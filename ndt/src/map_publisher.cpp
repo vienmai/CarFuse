43060b07060d2da6607e94916db55f870fcc5521
Vien Mai <mienvanmaidt2@gmail.com>
2023-02-05 15:25:43 +0100

    Refactor map publisher
    - Use declare parameter
    - Rename the publisher node
    - Fix typos

--- ndt/src/map_publisher.cpp
+++ ndt/src/map_publisher.cpp
@@ -3,8 +3,8 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 MapPublisher::MapPublisher() : Node("map_publisher") {
-  std::string map_topic =
-      this->get_parameter_or("map_topic", std::string("/map"));
+  auto map_topic = declare_parameter("map_topic", std::string("/map"));
+  get_parameter("map_topic", map_topic);
   RCLCPP_INFO(get_logger(), "Map topic: %s", map_topic.c_str());
   map_pub_ =
       this->create_publisher<sensor_msgs::msg::PointCloud2>(map_topic, 1);
@@ -21,8 +21,8 @@ void MapPublisher::publish_map(const std::string &path) {
   // Convert the PointCloud to a PointCloud2 message
   sensor_msgs::msg::PointCloud2 map_msg;
   pcl::toROSMsg(*map_cloud, map_msg);
-  std::string map_frame =
-      this->get_parameter_or("map_frame", std::string("map"));
+  auto map_frame = declare_parameter("map_frame", std::string("map_frame"));
+  get_parameter("map_frame", map_frame);
   map_msg.header.frame_id = map_frame;
   RCLCPP_INFO(get_logger(), "Map message header id: %s", map_frame.c_str());
 
--- ndt/src/map_publisher_node.cpp
+++ ndt/src/map_publisher_node.cpp
@@ -1,35 +1,26 @@
-#include <pcl_conversions/pcl_conversions.h>
-
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include "ndt/map_publisher.hpp"
-
-using namespace localization;
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
 
   // Create an instance the MapPublisher node
-  auto map_publisher_node = std::make_shared<MapPublisher>();
+  auto node = std::make_shared<MapPublisher>();
 
   // Read the configuration file
-  std::string map_file = map_publisher_node->get_parameter_or(
-      "map_file", std::string("/ros2_ws/install/ndt_locator/share/ndt_locator/"
-                              "maps/court_yard_map.pcd")
-  );
-  RCLCPP_INFO(
-      map_publisher_node->get_logger(), "Map file: %s", map_file.c_str()
-  );
+  auto map_file = node->declare_parameter("map_file", std::string("map.pcd"));
+  node->get_parameter("map_file", map_file);
+  RCLCPP_INFO(node->get_logger(), "Map file: %s", map_file.c_str());
 
   // Publish the map to the map topic
-  map_publisher_node->publish_map(map_file);
+  node->publish_map(map_file);
 
-  // Publish the initial pose
+  // Generate an initial pose
   geometry_msgs::msg::PoseStamped initial_pose_msg;
   initial_pose_msg.header.stamp = rclcpp::Clock().now();
   initial_pose_msg.header.frame_id = "map";
-
-  // Set the initial pose here, for example:
   initial_pose_msg.pose.position.x = 0;
   initial_pose_msg.pose.position.y = 0;
   initial_pose_msg.pose.position.z = 0;
@@ -40,13 +31,13 @@ int main(int argc, char *argv[]) {
 
   // Publish the initial pose
   auto initial_pose_publisher =
-      ndt_node->create_publisher<geometry_msgs::msg::PoseStamped>(
+      node->create_publisher<geometry_msgs::msg::PoseStamped>(
           "/initial_pose", 1
       );
   initial_pose_publisher->publish(initial_pose_msg);
-  RCLCPP_INFO(ndt_node->get_logger(), "Initial pose published.");
+  RCLCPP_INFO(node->get_logger(), "Initial pose published.");
 
-  rclcpp::spin(ndt_node);
+  rclcpp::spin(node);
   rclcpp::shutdown();
 
   return 0;
