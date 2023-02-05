396f78857f98a4e591972ec63e13c0ec094e5dbe
Vien Mai <mienvanmaidt2@gmail.com>
2023-02-05 15:20:39 +0100

    Update ndt.launch file

--- ndt/launch/ndt.launch.py
+++ ndt/launch/ndt.launch.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
+import os
 import launch
 import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='ndt_locator',
-            executable='ndt_node',
-            output='screen',
-            parameters=[{'map_file': 'maps/map.pcd'}]
-        ),
+    """Launch file."""
+    config_file = os.path.join(
+        get_package_share_directory('ndt_locator'),
+        'config/config.yaml'
+    )
+
+    tf = launch_ros.actions.Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=[
+            '0', '0', '0', '0', '0', '0', '1', 'base_link', 'os_sensor'
+        ]
+    )
+
+    map_publisher_node = launch_ros.actions.Node(
+        package='ndt_locator',
+        executable='map_publisher_node',
+        parameters=[config_file],
+        output='screen'
+    )
+
+    ndt_locator_node = launch_ros.actions.Node(
+        package='ndt_locator',
+        executable='ndt_node',
+        parameters=[config_file],
+        remappings=[('/scan', '/os_cloud_node/points'),],
+        output='screen'
+    )
+
+    ld = launch.LaunchDescription([
+        tf,
+        map_publisher_node,
+        ndt_locator_node,
     ])
+
+    return ld
