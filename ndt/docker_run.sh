1bc56ba25f5396c5d24911ab1afed7d147279fd8
Vien Mai <mienvanmaidt2@gmail.com>
2023-02-16 16:01:00 +0100

    Update docker run w/ map volume

--- ndt/docker_run.sh
+++ ndt/docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm \
-    --name ndt_dev \
+    --name ndt_locator \
     -v "$(pwd):/ros2_ws/src/ndt" \
+    -v "/Users/eviemai/Data:/ros2_ws/src/ndt/maps:ro" \
     ndt:dev
-    # -w /ros2_ws/src/ndt \
